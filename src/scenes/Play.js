dd918fd0b75aeb32bc08348f214fad0d3cc05391
mphannav <103499806+mphannav@users.noreply.github.com>
2022-04-12 13:55:37 -0700

    Update Play.js

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,10 +3,10 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
     preload(){
-        this.load.image('rocket', '/assets/rocket.png');
-        this.load.image('spaceship', '/assets/spaceship.png');
-        this.load.image('starfield', '/assets/starfield.png');
-        this.load.spritesheet('explosion', '/assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
 
     create(){
